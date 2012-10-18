64629dafc248cb8330d1e7e15779994977dc27b5
maslakov <mikhail.maslakov@gmail.com>
2012-10-18 13:58:10 +0400

    GELF bad header fixed

--- Gelf4net/Transport/UdpTransport.cs
+++ Gelf4net/Transport/UdpTransport.cs
@@ -1,80 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Esilog.Gelf4net.Transport
 {
     public class UdpTransport : GelfTransport
     {
-        public int MaxChunkSize { get; set; }
-		
-		private int _maxHeaderSize = 8;
-
-        public override void Send(string serverHostName, string serverIpAddress, int serverPort, string message)
+        public Int32 MaxChunkSize
         {
-            var ipAddress = IPAddress.Parse(serverIpAddress);
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, serverPort);
+            get;
+            set;
+        }
 
-            using(UdpClient udpClient = new UdpClient()){
-                var gzipMessage = GzipMessage(message);
+        private Int32 _maxHeaderSize = 8;
 
-                if (MaxChunkSize < gzipMessage.Length)
+        public override void Send( String serverHostName, String serverIpAddress, Int32 serverPort, String message )
+        {
+            var ipAddress = IPAddress.Parse( serverIpAddress );
+            IPEndPoint ipEndPoint = new IPEndPoint( ipAddress, serverPort );
+
+            using( UdpClient udpClient = new UdpClient() )
+            {
+                var gzipMessage = GzipMessage( message );
+
+                if( MaxChunkSize < gzipMessage.Length )
                 {
                     var chunkCount = (gzipMessage.Length / MaxChunkSize) + 1;
-                    var messageId = GenerateMessageId(serverHostName);
-                    for (int i = 0; i < chunkCount; i++)
+                    var messageId = GenerateMessageId( serverHostName );
+                    for( Int32 i = 0; i < chunkCount; i++ )
                     {
-                        var messageChunkPrefix = CreateChunkedMessagePart(messageId, i, chunkCount);
+                        var messageChunkPrefix = CreateChunkedMessagePart( messageId, i, chunkCount );
                         var skip = i * MaxChunkSize;
-                        var messageChunkSuffix = gzipMessage.Skip(skip).Take(MaxChunkSize).ToArray<byte>();
+                        var messageChunkSuffix = gzipMessage.Skip( skip ).Take( MaxChunkSize ).ToArray();
 
-                        var messageChunkFull = new byte[messageChunkPrefix.Length + messageChunkSuffix.Length];
-                        messageChunkPrefix.CopyTo(messageChunkFull, 0);
-                        messageChunkSuffix.CopyTo(messageChunkFull, messageChunkPrefix.Length);
+                        var messageChunkFull = new Byte[messageChunkPrefix.Length + messageChunkSuffix.Length];
+                        messageChunkPrefix.CopyTo( messageChunkFull, 0 );
+                        messageChunkSuffix.CopyTo( messageChunkFull, messageChunkPrefix.Length );
 
-                        udpClient.Send(messageChunkFull, messageChunkFull.Length, ipEndPoint);
+                        udpClient.Send( messageChunkFull, messageChunkFull.Length, ipEndPoint );
                     }
                 }
                 else
                 {
-                    udpClient.Send(gzipMessage, gzipMessage.Length, ipEndPoint);
+                    udpClient.Send( gzipMessage, gzipMessage.Length, ipEndPoint );
                 }
             }
         }
 
-        public byte[] CreateChunkedMessagePart(string messageId, int index, int chunkCount)
+        public Byte[] CreateChunkedMessagePart( String messageId, Int32 index, Int32 chunkCount )
         {
-            var result = new List<byte>();
-            var gelfHeader = new byte[2] { Convert.ToByte(30), Convert.ToByte(15) };
-            result.AddRange(gelfHeader);
-            result.Add(Convert.ToByte(30));
-            result.Add(Convert.ToByte(15));
-            result.AddRange(Encoding.Default.GetBytes(messageId).ToArray<byte>());
-            result.Add(Convert.ToByte(index));
-			result.Add(Convert.ToByte(chunkCount));
+            var result = new List<Byte>();
+            result.Add( Convert.ToByte( 30 ) );
+            result.Add( Convert.ToByte( 15 ) );
+            result.AddRange( Encoding.Default.GetBytes( messageId ).ToArray() );
+            result.Add( Convert.ToByte( index ) );
+            result.Add( Convert.ToByte( chunkCount ) );
 
-            return result.ToArray<byte>();
+            return result.ToArray<Byte>();
         }
 
-        public string GenerateMessageId(string serverHostName)
+        public String GenerateMessageId( String serverHostName )
         {
-            var md5String = String.Join("", MD5.Create().ComputeHash(Encoding.Default.GetBytes(serverHostName)).Select(it => it.ToString("x2")).ToArray<string>());
-            var random = new Random((int)DateTime.Now.Ticks);
+            var md5String = String.Join(
+                "",
+                MD5.Create().ComputeHash( Encoding.Default.GetBytes( serverHostName ) ).Select(
+                    it => it.ToString( "x2" ) ).ToArray() );
+            var random = new Random( (Int32)DateTime.Now.Ticks );
             var sb = new StringBuilder();
             var t = DateTime.Now.Ticks % 1000000000;
-            var s = String.Format("{0}{1}", md5String.Substring(0, 10), md5String.Substring(20, 10));
-            var r = random.Next(10000000).ToString("00000000");
+            var s = String.Format( "{0}{1}", md5String.Substring( 0, 10 ), md5String.Substring( 20, 10 ) );
+            var r = random.Next( 10000000 ).ToString( "00000000" );
 
-            sb.Append(t);
-            sb.Append(s);
-            sb.Append(r);
-			
-			//Message ID: 8 bytes 
-            return sb.ToString().Substring(0, _maxHeaderSize) ;
+            sb.Append( t );
+            sb.Append( s );
+            sb.Append( r );
+
+            //Message ID: 8 bytes 
+            return sb.ToString().Substring( 0, _maxHeaderSize );
         }
     }
 }
