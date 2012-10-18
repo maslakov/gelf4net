d4fa65c81b5dc0d1496ec4f095f9d1e76b2f08ad
maslakov <mikhail.maslakov@gmail.com>
2012-10-18 14:03:31 +0400

    changed in order to  generate constant high load to graylog2 server instead of click-driven

--- RandomSentence/RandomSentenceForm.cs
+++ RandomSentence/RandomSentenceForm.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using log4net;
 
@@ -13,93 +9,135 @@ namespace SandboxWindow
 {
     public partial class RadomSentenceForm : Form
     {
-        protected static readonly ILog log = LogManager.GetLogger(typeof(RadomSentenceForm));
+        protected static readonly ILog log = LogManager.GetLogger( typeof( RadomSentenceForm ) );
+        private static Boolean isRun;
+        private TaskScheduler scheduler;
+        private CancellationTokenSource tokensource;
+        private CancellationToken token;
+        private Object rootLock = new Object();
+        private Task maintask;
+        private Random rand = new Random(unchecked((Int32)DateTime.Now.Ticks));
 
         public RadomSentenceForm()
         {
             log4net.Config.XmlConfigurator.Configure();
             InitializeComponent();
-			this.FormBorderStyle =  FormBorderStyle.Fixed3D;
+            scheduler = TaskScheduler.FromCurrentSynchronizationContext();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load( Object sender, EventArgs e )
         {
-            log.Debug("Starting the randomizer sentence");
-			Size = new Size(800, 600);
-            RandomizeSentence();
+            log.Debug( "Starting the randomizer sentece" );
+            label1.Text = RandomizeSentence();
         }
-		
-		private string GetPath(string path, string file)
-		{
-			return String.Concat(path, Path.DirectorySeparatorChar, file);
-		}
 
-        private void RandomizeSentence()
+        private String RandomizeSentence()
         {
-            var syntaxes = File.ReadAllLines(GetPath("Resources", "syntax.txt"));
-            var nouns = File.ReadAllLines(GetPath("Resources", "nouns.txt"));
-            var verbs = File.ReadAllLines(GetPath("Resources", "verbs.txt"));
-            var adjectives = File.ReadAllLines(GetPath("Resources", "adjectives.txt"));
-            var adverbs = File.ReadAllLines(GetPath("Resources", "adverbs.txt"));
-            var rand = new Random();
+            ThreadContext.Properties["requestId"] = Guid.NewGuid().ToString();
 
-            var sentence = syntaxes[rand.Next(syntaxes.Length)];
+            var syntaxes = File.ReadAllLines( @"Resources\syntax.txt" );
+            var nouns = File.ReadAllLines( @"Resources\nouns.txt" );
+            var verbs = File.ReadAllLines( @"Resources\verbs.txt" );
+            var adjectives = File.ReadAllLines( @"Resources\adjectives.txt" );
+            var adverbs = File.ReadAllLines( @"Resources\adverbs.txt" );
+            
+
+            var sentence = syntaxes[rand.Next( syntaxes.Length )];
             var complete = false;
-            while (!complete)
+            while( !complete )
             {
-                var index = sentence.IndexOf("[Noun]");
-                if (index > 0)
+                var index = sentence.IndexOf( "[Noun]" );
+                if( index > 0 )
                 {
-                    sentence = sentence.Remove(index, "[Noun]".Length);
-                    sentence = sentence.Insert(index, nouns[rand.Next(nouns.Length)].Trim());
+                    sentence = sentence.Remove( index, "[Noun]".Length );
+                    sentence = sentence.Insert( index, nouns[rand.Next( nouns.Length )].Trim() );
                     continue;
                 }
-                index = sentence.IndexOf("[Verb]");
-                if (index > 0)
+                index = sentence.IndexOf( "[Verb]" );
+                if( index > 0 )
                 {
-                    sentence = sentence.Remove(index, "[Verb]".Length);
-                    sentence = sentence.Insert(index, verbs[rand.Next(verbs.Length)].Trim());
+                    sentence = sentence.Remove( index, "[Verb]".Length );
+                    sentence = sentence.Insert( index, verbs[rand.Next( verbs.Length )].Trim() );
                     continue;
                 }
-                index = sentence.IndexOf("[Adverb]");
-                if (index > 0)
+                index = sentence.IndexOf( "[Adverb]" );
+                if( index > 0 )
                 {
-                    sentence = sentence.Remove(index, "[Adverb]".Length);
-                    sentence = sentence.Insert(index, adverbs[rand.Next(adverbs.Length)].Trim());
+                    sentence = sentence.Remove( index, "[Adverb]".Length );
+                    sentence = sentence.Insert( index, adverbs[rand.Next( adverbs.Length )].Trim() );
                     continue;
                 }
-                index = sentence.IndexOf("[Adjective]");
-                if (index > 0)
+                index = sentence.IndexOf( "[Adjective]" );
+                if( index > 0 )
                 {
-                    sentence = sentence.Remove(index, "[Adjective]".Length);
-                    sentence = sentence.Insert(index, adjectives[rand.Next(adjectives.Length)].Trim());
+                    sentence = sentence.Remove( index, "[Adjective]".Length );
+                    sentence = sentence.Insert( index, adjectives[rand.Next( adjectives.Length )].Trim() );
                     continue;
                 }
                 complete = true;
             }
             var cutIndex = 0;
             var readChars = 0;
-            const int maxWidth = 32;
-            while (sentence.Length > readChars + maxWidth)
+            const Int32 maxWidth = 32;
+            while( sentence.Length > readChars + maxWidth )
             {
-                cutIndex = sentence.Substring(cutIndex, maxWidth).LastIndexOf(" ");
-                sentence = sentence.Remove(readChars + cutIndex, 1);
-                sentence = sentence.Insert(readChars + cutIndex, "\n");
+                cutIndex = sentence.Substring( cutIndex, maxWidth ).LastIndexOf( " " );
+                sentence = sentence.Remove( readChars + cutIndex, 1 );
+                sentence = sentence.Insert( readChars + cutIndex, "\n" );
                 readChars += cutIndex;
             }
-            label1.Text = sentence.ToUpper();
-
-            log.Debug(String.Format("Randomizer Sentence: {0}", label1.Text));
+            var result = sentence.ToUpper();
+            log.Debug( String.Format( "Randomizer Sentence: {0}", result ) );
+            return result;
         }
 
-        private void Form1_Click(object sender, EventArgs e)
+        private void Form1_Click( Object sender, EventArgs e )
         {
-            RandomizeSentence();
+            lock( rootLock )
+            {
+                if( !isRun )
+                {
+                    tokensource = new CancellationTokenSource();
+                    token = tokensource.Token;
+
+                    this.Text = "Running...";
+                    log.Info( "Start running..." );
+                    var task = Task.Factory.StartNew(
+                        () =>
+                        {
+                            while( ! token.IsCancellationRequested )
+                            {
+                                RandomizeSentence();
+                            }
+                        }, token );
+
+                    maintask = task.ContinueWith(
+                            finished =>
+                            {
+                                if( finished.Exception != null )
+                                {
+                                    log.Error( finished.Exception.Flatten() );
+                                }
+                            } );
+                    isRun = true;
+                }
+                else
+                {
+                    tokensource.Cancel();
+                    this.Text = "Waiting...";
+                    log.Info("Waiting for stop...");
+                    
+                    maintask.Wait();
+                    
+                    log.Info("Stopped...");
+                    isRun = false;
+                }
+            }
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void Form1_FormClosing( Object sender, FormClosingEventArgs e )
         {
-            log.Debug("Closing the app");
+            log.Debug( "Closing the app" );
         }
     }
 }
